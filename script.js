6a2429066295de9dcac05aebe61991e8ac1a3cc8
frayrody <121489841+frayrody@users.noreply.github.com>
2024-07-13 11:27:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.content');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+    
+    document.getElementById(sectionId).style.display = 'flex';
+}
